2fc83f465a1915161f3f8ea8e89f900af1984798
Brandon Bounsophinh <36938689+Beninix@users.noreply.github.com>
2023-10-14 17:39:02 -0400

    Update main.py
    Added file reading for colour definitions

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pyautogui
 from pymsgbox import *
+names = []
+codes = []
+colours = open("basic colours.txt", "r").readlines()
+for colour in range(len(colours)):
+    colours[colour]=colours[colour].rstrip("\n")
+print(colours)
+
+
 screen = pyautogui.screenshot()
 pixel = screen.getpixel(pyautogui.position())
-alert(text=str(pixel), title ="Color", button="Ok")
+alert(text=str(pixel), title ="Colour", button="Ok")
