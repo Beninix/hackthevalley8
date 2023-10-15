4c8effcf5fca22e5a9da7d114be0f38079b2c230
Brandon Bounsophinh <36938689+Beninix@users.noreply.github.com>
2023-10-14 20:57:33 -0400

    Update main.py
    Added input of the data from the file read and started distance checking

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import pyautogui
 from pymsgbox import *
+
+#Initialize
 names = []
 codes = []
+#Open and read file
 colours = open("basic colours.txt", "r").readlines()
-for colour in range(len(colours)):
-    colours[colour]=colours[colour].rstrip("\n")
-print(colours)
-
-
+for i in range(len(colours)):
+    #Clean endline markers
+    colours[i]=colours[i].rstrip("\n")
+    #Break string into desired data
+    temp = colours[i].split("|")
+    #Assign data to 2 parallel arrays
+    names.append(temp[0])
+    codes.append(temp[1].strip("()").split(","))
+print(codes)
+#Color checking
 screen = pyautogui.screenshot()
 pixel = screen.getpixel(pyautogui.position())
+
+#Match color code to color name
+closest = 0 #Index of closest
+closestDist = pixel
+
+print(pixel)
 alert(text=str(pixel), title ="Colour", button="Ok")
