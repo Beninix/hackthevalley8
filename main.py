c4eefbe591cff6442c0bb249d467ddc3b203f194
Brandon Bounsophinh <36938689+Beninix@users.noreply.github.com>
2023-10-15 01:26:33 -0400

    Update main.py
    Added config, cleaned up and made sure things were commented

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from pymsgbox import *
 import keyboard
 
 def dist(point1, point2):
+    '''
+    Calculates 3D Euclidian distance (without sqrt)
+    Parameters:
+        point1(int[]): First point
+        point2(int[]): Second point
+    Returns:
+        distance(int)
+    '''
     total = 0
     for i in range(3):
         total += (point1[i]-point2[i])**2
@@ -11,8 +19,22 @@ def dist(point1, point2):
 #Initialize
 names = []
 codes = []
-#Open and read file
-colours = open("basic colours.txt", "r").readlines()
+
+#Open config
+config = open("config.txt", "r").readlines()
+
+#Set the activation key
+key = config[1].split("=")[1].strip()
+
+#Detailed Colours
+if(config[0].split("=")[1].strip()=="Yes"):
+    colours = open("detailed colours.txt", "r").readlines()
+    print("YIPEEE")    
+#Basic Colours
+else:
+    colours = open("basic colours.txt", "r").readlines()
+    
+#Open and read colour definitions
 for i in range(len(colours)):
     #Clean endline markers
     colours[i]=colours[i].rstrip("\n")
@@ -24,19 +46,21 @@ for i in range(len(colours)):
     #Convert to int
     for j in range(3):
         codes[i][j]=int(codes[i][j])
+
+#Colour checking
 while(True):
-    keyboard.wait("a")
-    #Color checking
+    keyboard.wait(key)
+    #Read the pixel the mouse is at
     screen = pyautogui.screenshot()
     pixel = screen.getpixel(pyautogui.position())
-
     #Match color code to color name
     closest = 0 #Index of closest
     closestDist = dist(pixel, codes[0])
-    print(pixel)
+    #Check which colour is the closest
     for i in range(len(codes)):
         newDist = dist(pixel,codes[i])
         if(newDist<closestDist):
             closestDist = newDist
             closest = i
+    #Send the message
     alert(text=str(names[closest]), title ="Colour", button="Ok")
